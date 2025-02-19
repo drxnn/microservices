c644b5edfc8eadada5412353a0637b2c5b9b0d33
Drin Bratotini <drin.bratotini@gmail.com>
2025-02-19 14:00:46 -0500

    signUp done

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -19,17 +19,29 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 // using function keyword so I can access this keyword
 // a pre hook runs before a document is saved to a database
